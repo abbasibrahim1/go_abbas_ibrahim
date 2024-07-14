26d12469ab00cd95231ee15eb408618943a7b181
abbasibrahim1 <abbasshuosha@gmail.com>
2024-07-14 20:50:44 +0700

    submitting assignment for time and space complexity

---
+++ Data Strocture/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() 
+ // slice -> works like array, but with flexible size
+
+ // approach 1
+ var book [] string 
+
+ // add items -> append ()
+ books = append(books, "Data Science")
+ books = append (books, "Algorithm", "Ditgital Marketing", " full stack Dev")
+ 
+
